32c02267559afc4151ba580362a1890da4397394
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-11 19:32:22 -0300

    delete implementation

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
+import db.DB;
 import models.dao.DaoFactory;
 import models.dao.SellerDao;
 import models.entities.Department;
@@ -9,6 +11,9 @@ import models.entities.Seller;
 public class Main {
 
 	public static void main(String[] args) {
+
+		System.out.println("=== SELLER TESTS ===");
+		System.out.println();
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -36,13 +41,38 @@ public class Main {
 		seller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(seller);
 		System.out.println("Inserted: New id = " + seller.getId());
-		
+
+		System.out.println();
+
 		System.out.println("=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Joao Blue");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 
+		System.out.println();
+
+		System.out.println("=== TEST 6: seller delete ===");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.print("Delete completed");
+		sc.close();
+
+		DB.closeConnection();
+
+		System.out.println();
+
+		System.out.println("=== DEPARTMENT TESTS ===");
+		System.out.println();
+
+		System.out.println("=== TEST 1: department findById ===");
+
+		// System.out.println("=== TEST 2: department findAll ===");
+		// System.out.println("=== TEST 3: department insert ===");
+		// System.out.println("=== TEST 4: department update ===");
+		// System.out.println("=== TEST 5: department delete ===");
 
 	}
 
---
+++ models/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,14 @@
+package models.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import models.dao.DepartmentDao;
+import models.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	
+
+}
