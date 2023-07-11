88550861c1d9620973d45d368ff0617273270f26
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-11 18:28:42 -0300

    findById implementation

--- Main.java
+++ Main.java
@@ -1,22 +1,16 @@
-import java.time.LocalDate;
-
 import models.dao.DaoFactory;
 import models.dao.SellerDao;
-import models.entities.Department;
 import models.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Department dep = new Department(1, "Department");
-		LocalDate date = LocalDate.now();
-		Seller seller = new Seller(1, "Name", "name@email.com", date, 1200.00, dep);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
-		
+
 	}
 
 }
