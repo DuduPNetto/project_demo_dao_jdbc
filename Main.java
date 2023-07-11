5f4d4a174d3a2d647958488a1cb588a7d35a6d6a
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-11 18:01:08 -0300

    Seller class

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
+import java.time.LocalDate;
+
+import models.entities.Department;
+import models.entities.Seller;
+
 public class Main {
 
 	public static void main(String[] args) {
+	
+		Department dep = new Department(1, "Department");
+		LocalDate date = LocalDate.now();
+		Seller seller = new Seller(1, "Name", "name@email.com", date, 1200.00, dep);
 		
-		
+		System.out.println(seller);
 		
 	}
-	
+
 }
